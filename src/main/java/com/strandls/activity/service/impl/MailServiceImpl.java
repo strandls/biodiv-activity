f739a64cb2b00f230736d3e675b2656704a74b51
sethu1012 <sethu10121994@gmail.com>
2020-03-30 17:31:38 +0530

    :recycle: Moved data to a generic function

--- src/main/java/com/strandls/activity/service/impl/MailServiceImpl.java
+++ src/main/java/com/strandls/activity/service/impl/MailServiceImpl.java
@@ -36,7 +36,7 @@ public class MailServiceImpl implements MailService {
 	private static final Logger logger = LoggerFactory.getLogger(MailServiceImpl.class);
 	private String siteName = "";
 	private String serverUrl = "";
-	
+
 	@Inject
 	private RabbitMQProducer producer;
 
@@ -80,61 +80,78 @@ public class MailServiceImpl implements MailService {
 			if (userGroupActivityList.contains(activity.getActivityType())) {
 				userGroup = mapper.readValue(activity.getActivityDescription(), UserGroupActivity.class);
 			}
+			Map<String, Object> data = null;
+			// Send to followers
 			for (Recipients recipient : recipientsList) {
 				if (recipient.getIsSubscribed() != null && recipient.getIsSubscribed()) {
 					User follower = userService.getUser(String.valueOf(recipient.getId()));
-					Map<String, Object> data = new HashMap<String, Object>();
-					data.put(FIELDS.TYPE.getAction(), type.getAction());
-					data.put(FIELDS.TO.getAction(), new String[] { recipient.getEmail() });
-					Map<String, Object> model = new HashMap<String, Object>();
-					model.put(COMMENT_POST.TYPE.getAction(), type.getAction());
-					model.put(COMMENT_POST.SITENAME.getAction(), siteName);
-					model.put(COMMENT_POST.SERVER_URL.getAction(), serverUrl);
-					model.put(SUGGEST_MAIL.RECO_VOTE.getAction(), name);
-					if (comment != null) {
-						model.put(COMMENT_POST.COMMENT_BODY.getAction(), comment.getBody());
-					}
-					if (type == MAIL_TYPE.FACT_UPDATED || type == MAIL_TYPE.TAG_UPDATED
-							|| type == MAIL_TYPE.CUSTOM_FIELD_UPDATED) {
-						model.put(COMMENT_POST.COMMENT_BODY.getAction(), activity.getActivityDescription());
-					}
-					model.put(COMMENT_POST.FOLLOWER_ID.getAction(), follower.getId());
-					model.put(COMMENT_POST.FOLLOWER_NAME.getAction(), follower.getName());
-					model.put(COMMENT_POST.WHO_POSTED_ID.getAction(), who.getId());
-					model.put(COMMENT_POST.WHO_POSTED_ICON.getAction(), who.getIcon() == null ? "" : who.getIcon());
-					model.put(COMMENT_POST.WHO_POSTED_NAME.getAction(),
-							follower.getId() == who.getId() ? "You" : who.getName());
-					if (reco != null) {
-						model.put(SUGGEST_MAIL.GIVEN_NAME_ID.getAction(),
-								reco.getSpeciesId() == null ? 0 : reco.getSpeciesId());
-						model.put(SUGGEST_MAIL.GIVEN_NAME_NAME.getAction(), name);
-						model.put(SUGGEST_MAIL.GIVEN_NAME_IS_SCIENTIFIC_NAME.getAction(),
-								reco.getScientificName() != null || !reco.getScientificName().isEmpty());
-					}
-					model.put(COMMENT_POST.WHAT_POSTED_ID.getAction(), observation.getObservationId());
-					model.put(COMMENT_POST.WHAT_POSTED_NAME.getAction(),
-							observation.getCommonName() == null ? "" : observation.getCommonName());
-					model.put(COMMENT_POST.WHAT_POSTED_LOCATION.getAction(),
-							observation.getLocation() == null ? "" : observation.getLocation());
-					model.put(COMMENT_POST.WHAT_POSTED_OBSERVED_ON.getAction(), observation.getObservedOn());
-					model.put(COMMENT_POST.WHAT_POSTED_ICON.getAction(),
-							observation.getIconURl() == null ? "" : observation.getIconURl());
-					model.put(COMMENT_POST.WHAT_POSTED_USERGROUPS.getAction(), groups);
-					if (userGroup != null) {
-						model.put(POST_TO_GROUP.WHERE_WEB_ADDRESS.getAction(), userGroup.getWebAddress());
-						model.put(POST_TO_GROUP.WHERE_USER_GROUPNAME.getAction(), userGroup.getUserGroupName());
-					}
-					model.put(POST_TO_GROUP.SUBMIT_TYPE.getAction(),
-							activity.getActivityType().toLowerCase().contains("post") ? "post" : "");
-					data.put(FIELDS.DATA.getAction(), JsonUtil.unflattenJSON(model));
+					data = prepareMailData(type, follower, who, reco, userGroup, activity, comment,
+							name, observation, groups); 
 					producer.produceMail(RabbitMqConnection.EXCHANGE, RabbitMqConnection.ROUTING_KEY, null,
 							JsonUtil.mapToJSON(data));
 				}
 			}
+			String admins = PropertyFileUtil.fetchProperty("config.properties", "mail_bcc");
+			data.put(FIELDS.TO.getAction(), admins.split(","));
+			producer.produceMail(RabbitMqConnection.EXCHANGE, RabbitMqConnection.ROUTING_KEY, null,
+					JsonUtil.mapToJSON(data));			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			logger.error(ex.getMessage());
 		}
 	}
 
+	private Map<String, Object> prepareMailData(
+			MAIL_TYPE type, User follower, User who, RecoVoteActivity reco, UserGroupActivity userGroup, 
+			ActivityLoggingData activity, CommentLoggingData comment, String name, observationMailData observation,
+			List<UserGroupMailData> groups) {
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put(FIELDS.TYPE.getAction(), type.getAction());
+		data.put(FIELDS.TO.getAction(), PropertyFileUtil.fetchProperty("config.properties", "temp_email").split(","));
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put(COMMENT_POST.TYPE.getAction(), type.getAction());
+		model.put(COMMENT_POST.SITENAME.getAction(), siteName);
+		model.put(COMMENT_POST.SERVER_URL.getAction(), serverUrl);
+		model.put(SUGGEST_MAIL.RECO_VOTE.getAction(), name);
+		if (comment != null) {
+			model.put(COMMENT_POST.COMMENT_BODY.getAction(), comment.getBody());
+		}
+		if (type == MAIL_TYPE.FACT_UPDATED || type == MAIL_TYPE.TAG_UPDATED || type == MAIL_TYPE.CUSTOM_FIELD_UPDATED) {
+			model.put(COMMENT_POST.COMMENT_BODY.getAction(), activity.getActivityDescription());
+		}
+		model.put(COMMENT_POST.FOLLOWER_ID.getAction(), follower.getId());
+		model.put(COMMENT_POST.FOLLOWER_NAME.getAction(), follower.getName());
+		model.put(COMMENT_POST.WHO_POSTED_ID.getAction(), who.getId());
+		model.put(COMMENT_POST.WHO_POSTED_ICON.getAction(), who.getIcon() == null ? "" : who.getIcon());
+		model.put(COMMENT_POST.WHO_POSTED_NAME.getAction(), follower.getId() == who.getId() ? "You" : who.getName());
+		if (reco != null) {
+			model.put(SUGGEST_MAIL.GIVEN_NAME_ID.getAction(), reco.getSpeciesId() == null ? 0 : reco.getSpeciesId());
+			model.put(SUGGEST_MAIL.GIVEN_NAME_NAME.getAction(), name);
+			model.put(SUGGEST_MAIL.GIVEN_NAME_IS_SCIENTIFIC_NAME.getAction(),
+					reco.getScientificName() != null || !reco.getScientificName().isEmpty());
+		}
+		model.put(COMMENT_POST.WHAT_POSTED_ID.getAction(), observation.getObservationId());
+		model.put(COMMENT_POST.WHAT_POSTED_NAME.getAction(),
+				observation.getCommonName() == null ? "" : observation.getCommonName());
+		model.put(COMMENT_POST.WHAT_POSTED_LOCATION.getAction(),
+				observation.getLocation() == null ? "" : observation.getLocation());
+		model.put(COMMENT_POST.WHAT_POSTED_OBSERVED_ON.getAction(), observation.getObservedOn());
+		String image = observation.getIconURl() == null ? "" : observation.getIconURl();
+		if (!image.isEmpty()) {
+			int dot = image.lastIndexOf(".");
+			String fileName = image.substring(0, dot);
+			String extension = image.substring(dot);
+			image = String.join("_th1", fileName, extension);
+		}
+		model.put(COMMENT_POST.WHAT_POSTED_ICON.getAction(), image);
+		model.put(COMMENT_POST.WHAT_POSTED_USERGROUPS.getAction(), groups);
+		if (userGroup != null) {
+			model.put(POST_TO_GROUP.WHERE_WEB_ADDRESS.getAction(), userGroup.getWebAddress());
+			model.put(POST_TO_GROUP.WHERE_USER_GROUPNAME.getAction(), userGroup.getUserGroupName());
+		}
+		model.put(POST_TO_GROUP.SUBMIT_TYPE.getAction(),
+				activity.getActivityType().toLowerCase().contains("post") ? "post" : "");
+		data.put(FIELDS.DATA.getAction(), JsonUtil.unflattenJSON(model));
+		return data;
+	}
 }
