41ae99f1fd70b7e1640d116e02bcdf74ac1078c1
Abhishek Kumar Rudra <abhishek.rudra02@gmail.com>
2020-05-25 18:33:36 +0530

    jersey2 transition

--- src/main/java/com/strandls/activity/service/impl/MailServiceImpl.java
+++ src/main/java/com/strandls/activity/service/impl/MailServiceImpl.java
@@ -12,7 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.inject.Inject;
+import javax.inject.Inject;
+
 import com.strandls.activity.RabbitMqConnection;
 import com.strandls.activity.pojo.ActivityLoggingData;
 import com.strandls.activity.pojo.CommentLoggingData;
